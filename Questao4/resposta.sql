1d365350c9e3c1924a19d0b59bbaa3fe01e5cbfe
Davi Alves <davi.sousa10@gmail.com>
2022-12-30 15:55:25 -0300

    ✅ - questao 4

---
+++ Questao4/resposta.sql
@@ -0,0 +1,7 @@
+﻿select assunto, ano, count(assunto) as "QUANTIDADE"
+    from atendimentos
+    group by assunto, ano
+    having count(assunto) > 3
+    order by 
+        ano desc,
+        count(assunto) desc 
